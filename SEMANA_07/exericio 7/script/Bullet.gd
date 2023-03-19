440a48f10f41c2bdcaddfe3d15814ec5224ced39
Gabriel de Macedo Santos <gabriel.macedo@sou.inteli.edu.br>
2023-03-19 09:29:50 -0300

    add: comentários que estavam faltando

--- SEMANA_07/exericio 7/script/Bullet.gd
+++ SEMANA_07/exericio 7/script/Bullet.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 var speed = 600
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	set_physics_process(true)
 
--- SEMANA_07/exericio 7/script/Player.gd
+++ SEMANA_07/exericio 7/script/Player.gd
@@ -1,31 +1,31 @@
 extends KinematicBody2D
 
+# Transformando a bala em objeto do game
 var bullet = preload("res://Assets/caracter-removebg-preview.png")
 var speed = 400
 
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	# Valores verdadeiros a funções que serão usadas.
 	set_physics_process(true)
 	set_process(true)
 
 
 func _process(delta):
 	# Spawnando a bala
+	# Comando adcionado com o "space"
 	if Input.is_action_just_pressed("fire"):
 		var Bullet_Instance = bullet.instance()
+		# Usando o vector2 para que a bala nasça na mesma posição x, porém acima do personagem
 		bullet.instance().position = Vector2(position.x, position.y - 60)
 		get_tree().get_root().add_child(Bullet_Instance)
 
 
 func _physics_process(delta):
+	# Verificando se o botão pressionado foi o esquerdo
 	if Input.is_action_pressed("ui_left"):
 		move_and_collide(Vector2(-speed * delta, 0))
-	
+	# Verificando se o botão pressionado foi o direito.
 	elif Input.is_action_pressed("ui_right"):
 		move_and_collide(Vector2(speed * delta, 0))
 
-
-func _on_BackButtom_pressed():
-	get_tree().change_scene("res://scenes/tela_inicial.tscn")
